e8fd28dfc3324a7b98cc5fe1c2399284422fd61e
Juan Manuel Moncada Orjuela <jumamon7@gmail.com>
2026-02-04 22:05:17 -0500

    creacion da tabla ejemplo

--- Mision2/PBpg/energia/src/main/java/com/talentotech/energia/model/User.java
+++ Mision2/PBpg/energia/src/main/java/com/talentotech/energia/model/User.java
@@ -4,9 +4,9 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id  //Id es primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //Id generado automaticamente y autoincrementa
+    private Long id; //Tipo y nombre de variable
     @Column(nullable = false,unique = true)
     private String username;
     @Column(nullable = false,unique = true)
